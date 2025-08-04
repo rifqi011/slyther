793e92d9830972aa1ec12f2d42a15cad46dcd591
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-08-04 20:16:12 +0700

    remove floating background shapes from Background component

--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -20,14 +20,6 @@ export default function Background() {
 					}}
 				></div>
 			</div>
-
-			{/* Floating Background Shapes */}
-			<div className="floating-shapes">
-				<div className="shape shape-1"></div>
-				<div className="shape shape-2"></div>
-				<div className="shape shape-3"></div>
-				<div className="shape shape-4"></div>
-			</div>
 		</>
 	)
 }
